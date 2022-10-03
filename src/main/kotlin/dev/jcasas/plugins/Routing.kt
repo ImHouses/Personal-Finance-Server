b4a29e6cf072a16417468429f3024dc02c003fd3
Juan Casas <imhouses19@gmail.com>
2022-10-02 19:26:09 -0500

    Initial commit

---
+++ src/main/kotlin/dev/jcasas/plugins/Routing.kt
@@ -0,0 +1,17 @@
+package dev.jcasas.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+
+        get("/") {
+            call.respond(mapOf("Text" to "Hello, World!"))
+        }
+    }
+}
