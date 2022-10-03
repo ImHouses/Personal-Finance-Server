9924dd35802f10b23830f1dea8f3de301ba3a799
Juan Casas <imhouses19@gmail.com>
2022-10-03 00:15:26 -0500

    Remove unused code

--- src/main/kotlin/dev/jcasas/plugins/Serialization.kt
+++ src/main/kotlin/dev/jcasas/plugins/Serialization.kt
@@ -4,7 +4,6 @@ import io.ktor.serialization.gson.gson
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.routing.get
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
