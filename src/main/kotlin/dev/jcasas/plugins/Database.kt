61387f6e073ece02b6b993a4024eed886896f68d
Juan Casas <imhouses19@gmail.com>
2022-10-14 18:14:27 -0500

    Add TODO for database connection

--- src/main/kotlin/dev/jcasas/plugins/Database.kt
+++ src/main/kotlin/dev/jcasas/plugins/Database.kt
@@ -4,6 +4,7 @@ import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 object Database {
+    // TODO: Get Database connection information from configuration file.
     val instance = with(KMongo.createClient().coroutine) {
         getDatabase("personal_finances")
     }
